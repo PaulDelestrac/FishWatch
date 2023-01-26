509afdc6e4c8f038cdda68a732bc6e11bb748515
Paul Delestrac <paul.delestrac@icloud.com>
2023-01-26 13:30:37 +0100

    Initial Commit

---
+++ FishWatch Watch App/FishWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishWatchApp.swift
+//  FishWatch Watch App
+//
+//  Created by Paul Delestrac on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FishWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishWatch/FishWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishWatchApp.swift
+//  FishWatch
+//
+//  Created by Paul Delestrac on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FishWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
